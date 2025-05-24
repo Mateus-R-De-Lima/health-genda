ddc297e9a9d3bc4d62925931a127e59c386007dd
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-05-24 00:04:50 -0300

    Projeto Inicial

---
+++ src/main/java/com/mateus/lima/health_genda/HealthGendaApplication.java
@@ -0,0 +1,13 @@
+package com.mateus.lima.health_genda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthGendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthGendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateus/lima/health_genda/HealthGendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.lima.health_genda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthGendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
